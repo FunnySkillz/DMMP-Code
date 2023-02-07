8b3825f72ea4a66992da15e17ab9238c412c5482
FunnySkillz <sebastijan.bogdan@outlook.com>
2023-02-07 23:00:19 +0100

    add basic property service call in frontend

---
+++ DMMP-Frontend/DMMP-Frontend/Services/PropertyService.cs
@@ -0,0 +1,35 @@
+﻿using DMMP_Frontend.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMMP_Frontend.Services
+{
+    class PropertyService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PropertyService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<Property>> GetPropertiesAsync()
+        {
+            var response = await _httpClient.GetAsync("https://localhost:7284/api/Property/GetAllProperties");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Failed to retrieve properties.");
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var properties = JsonConvert.DeserializeObject<List<Property>>(json);
+
+            return properties;
+        }
+    }
+}
