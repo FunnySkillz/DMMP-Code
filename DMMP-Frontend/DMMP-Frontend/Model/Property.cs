e708a50b9a5b1af4be8698c7e66fdc8c6e274523
FunnySkillz <sebastijan.bogdan@outlook.com>
2023-02-07 22:48:26 +0100

    add model classes

---
+++ DMMP-Frontend/DMMP-Frontend/Model/Property.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMMP_Frontend.Model
+{
+    class Property
+    {
+        public string PropertyName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public int AddressNumber { get; set; }
+        public string City { get; set; } = string.Empty;
+        public int PostalCode { get; set; }
+        public int FloorAmount { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
