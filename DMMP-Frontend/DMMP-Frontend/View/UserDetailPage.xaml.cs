d2845aece6284a28cbdbcdec24b741a21de6b49a
FunnySkillz <sebastijan.bogdan@outlook.com>
2023-03-05 12:54:04 +0100

    add nfc page and navbar

--- DMMP-Frontend/DMMP-Frontend/View/UserDetailPage.xaml.cs
+++ DMMP-Frontend/DMMP-Frontend/View/UserDetailPage.xaml.cs
@@ -1,8 +1,8 @@
 namespace DMMP_Frontend.View;
 
-public partial class NewPage1 : ContentPage
+public partial class UserDetailPage : ContentPage
 {
-	public NewPage1()
+	public UserDetailPage()
 	{
 		InitializeComponent();
 	}
